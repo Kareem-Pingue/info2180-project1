72f91dca7acfada30aada5e77e781c0a93cfbeb0
Kareem Pingue <kareempingue@gmail.com>
2022-10-27 21:32:39 -0500

    27.10.2022

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/* Add your JavaScript to this file */
+//Kareem Pingue 620146393
+
+window.addEventListener('load', function(){
+    let submitButton = document.querySelector('.newsletter button');
+    let message = document.querySelector('.message');
+    const emailField = document.getElementById('email');
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault();
+        let fieldValue = emailField.value;
+        if(fieldValue === ''){
+            message.innerHTML = "Please enter a valid email address";
+        }
+        else{
+            message.innerHTML = `Thank You! Your email address ${fieldValue} has been added`;
+            emailField.value = '';
+        }
+    });
+    console.log("JS Loaded")
+}, false);
